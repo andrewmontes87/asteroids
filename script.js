15c338b8ca1d5f014c29f5f7809bd73a2422ef7e
andrewmontes87 <andrew@Andrews-MacBook-Pro.local>
2014-10-19 16:22:04 -0700

    boss update

--- script.js
+++ script.js
@@ -7,6 +7,13 @@
 
 // Create Game globals
 var globals = {
+  message: {
+    txt: "",
+    pos: {
+      x: 10,
+      y: 25
+    }
+  },
   bulletSpeed: 2,
   canvasHeight: 500,
   canvasWidth: 900,
@@ -27,7 +34,7 @@ var globals = {
       c.entities.destroy(c.entities.all()[0]);
     }
     globals.score = 0;
-    document.getElementById('score').innerHTML = globals.score;
+    globals.message.txt = "Score: " + globals.score;
 
     // Create Player instance
     c.entities.create(Player, {
@@ -36,9 +43,17 @@ var globals = {
         y: globals.canvasHeight/2
       }
     });
-    // Create AsteroidGenerator intance 
-    c.entities.create(AsteroidGenerator, {});
+    // Create EnemyGenerator intance 
+    c.entities.create(EnemyGenerator, {});
 
+  },
+  winGame: function(c) {
+    while (c.entities.all().length) {
+      c.entities.destroy(c.entities.all()[0]);
+    }
+    globals.message.txt = "Winner! Score: " + globals.score;
+    globals.message.pos.x = globals.canvasWidth/2;
+    globals.message.pos.y = globals.canvasHeight/2;
   }
 };
 
@@ -47,6 +62,12 @@ var Game = function() {
   // Create Game instance
   this.c = new Coquette(this, "canvas", globals.canvasWidth, globals.canvasHeight, globals.canvasBgcolor);
   globals.newGame(this.c);
+
+  this.draw = function (ctx) {
+    ctx.fillStyle = "blue";
+    ctx.font = "bold 16px Arial";
+    ctx.fillText(globals.message.txt, globals.message.pos.x, globals.message.pos.y);
+  };
 
 };
 
@@ -80,22 +101,19 @@ var Player = function(game, settings) {
       var bulletVel = {
         x: 6 * forward.x,
         y: 6 * forward.y
-      }
-      if (game.c.entities.all(Bullet).length < 40) {
-         game.c.entities.create(Bullet, { 
-          size : { x:3 , y:3 }, 
-          angle: player.angle,
-          center: bulletCenter,
-          vel: bulletVel,
-          type: "Player",
-          color:"red"
-        });     
-      }
+      };
+      game.c.entities.create(Bullet, { 
+        size : { x:3 , y:3 }, 
+        angle: player.angle,
+        center: bulletCenter,
+        vel: bulletVel,
+        type: "Player",
+        color:"red"
+      });     
     }
   };
 
   this.update = function() {
-
     if (this.c.inputter.isDown(this.c.inputter.LEFT_MOUSE) && this.center !== this.mousePos) {
       this.mousePos = this.c.inputter.getMousePosition();
       var slope = (this.mousePos.y - this.center.y) / (this.mousePos.x - this.center.x);
@@ -122,8 +140,7 @@ var Player = function(game, settings) {
       if (this.center.x < globals.canvasWidth) {
         this.center.x += 5;
       }
-      console.log()
-    };    
+    }    
     // keyhandlers
     if (this.c.inputter.isDown(this.c.inputter.UP_ARROW)) {
       if (this.center.y > 5) {
@@ -167,8 +184,7 @@ var Player = function(game, settings) {
   };
 
   this.collision = function(other) {
-    if (other.name === 'Asteroid' || other.name === 'Bullet' && other.type === 'Enemy'){
-      this.c.entities.destroy(this);
+    if (other.name === 'Enemy' || other.name === 'Boss' || other.name === 'Bullet' && other.type === 'Enemy'){
       globals.newGame(this.c);
     }
   };
@@ -182,80 +198,303 @@ var Player = function(game, settings) {
   };
 };
 
-// AsteroidGenerator class
-var AsteroidGenerator = function(game, settings) {
-  this.name = 'AsteroidGenerator';
+// EnemyGenerator class
+var EnemyGenerator = function(game, settings) {
+  this.name = 'EnemyGenerator';
   this.c = game.c;
   for (var i in settings) {
     this[i] = settings[i];
   }
   this.age = 0;
+  this.enemyModels = {
+    basic: {
+      name: 'Enemy',
+      timestamp: 0,
+      center: {
+        x: globals.canvasWidth,
+        y: globals.CanvasHeight/2
+      },
+      path: {
+        x: globals.canvasWidth/2,
+        y: globals.canvasHeight/2
+      },
+      vel: {
+        x: -1,
+        y: 0
+      },
+      angle: (Math.random() > 0.5) ? 215 : 145,
+      angle_vel: 0.2,
+      health: 100,
+      size: { x:20 , y:20 },
+      color: 'magenta',
+      gun: {
+        age: 0,
+        shoot: function(shooter) {
+          var forward = globals.angleToVector(shooter.angle);     
+          var bulletCenter = {
+            x: shooter.center.x + 15 * forward.x,
+            y: shooter.center.y + 15 * forward.y
+          };
+          var bulletVel = {
+            x: 6 * forward.x,
+            y: 6 * forward.y
+          };
+          if (game.c.entities.all(Bullet).length < 40) {
+             game.c.entities.create(Bullet, { 
+              size : { x:3 , y:3 }, 
+              angle: shooter.angle,
+              center: bulletCenter,
+              vel: bulletVel,
+              type: "Enemy",
+              color:"black"
+            });     
+          }
+        }   
+      }
+    },
+    boss: {
+      name: 'Boss',
+      timestamp: 0,
+      center: {
+        x: globals.canvasWidth,
+        y: globals.canvasHeight/2
+      },
+      vel: {
+        x: -2,
+        y: 0
+      },
+      angle: 180,
+      angle_vel: 0.1,
+      health: 3000,
+      size: { x:150 , y:150 },
+      color: 'red',
+      gun: {
+        age: 0,
+        shoot: function(shooter) {
+          var forward = globals.angleToVector(shooter.angle);     
+          var forwardPlus = globals.angleToVector(shooter.angle+15);
+          var forwardNeg = globals.angleToVector(shooter.angle-15);
+          var bulletCenter = {
+            x: shooter.center.x + (shooter.size.x/2 + 10) * forward.x,
+            y: shooter.center.y + (shooter.size.y/2 + 10) * forward.y
+          };
+          var bulletCenterPlus = {
+            x: shooter.center.x + (shooter.size.x/2 + 10) * forward.x,
+            y: shooter.center.y + (shooter.size.y/2 + 8) * forward.y
+          };
+          var bulletCenterNeg = {
+            x: shooter.center.x + (shooter.size.x/2 + 10) * forward.x,
+            y: shooter.center.y + (shooter.size.y/2 + 12) * forward.y
+          };
+          var bulletVel = {
+            x: 6 * forward.x,
+            y: 6 * forward.y
+          };
+          var bulletVelPlus = {
+            x: 6 * forwardPlus.x,
+            y: 6 * forwardPlus.y
+          };
+          var bulletVelNeg = {
+            x: 6 * forwardNeg.x,
+            y: 6 * forwardNeg.y
+          };
+          game.c.entities.create(Bullet, { 
+            size : { x:3 , y:3 }, 
+            angle: shooter.angle,
+            center: bulletCenter,
+            vel: bulletVel,
+            type: "Enemy",
+            color:"black"
+          }); 
+          game.c.entities.create(Bullet, { 
+            size : { x:3 , y:3 }, 
+            angle: shooter.angle+15,
+            center: bulletCenterPlus,
+            vel: bulletVelPlus,
+            type: "Enemy",
+            color:"black"
+          }); 
+          game.c.entities.create(Bullet, { 
+            size : { x:3 , y:3 }, 
+            angle: shooter.angle-15,
+            center: bulletCenterNeg,
+            vel: bulletVelNeg,
+            type: "Enemy",
+            color:"black"
+          });     
+        }   
+      }
+    },
+  };
+  this.waves = [
+    {
+      times: [0, 200],
+      center: { 
+        x: globals.canvasWidth,
+        y: globals.canvasHeight/4
+      },
+      path: {
+        x: globals.canvasWidth/2,
+        y: globals.canvasHeight/2
+      }
+    },
+    {
+      times: [500, 700],
+      center: { 
+        x: globals.canvasWidth,
+        y: globals.canvasHeight - globals.canvasHeight/4
+      },
+      path: {
+        x: globals.canvasWidth/2,
+        y: globals.canvasHeight/2
+      }
+    },
+    {
+      times: [1000, 1200],
+      center: { 
+        x: globals.canvasWidth,
+        y: globals.canvasHeight/4
+      },
+      path: {
+        x: globals.canvasWidth/2,
+        y: globals.canvasHeight/2
+      }
+    },
+    {
+      times: [1500, 1700],
+      center: { 
+        x: globals.canvasWidth,
+        y: globals.canvasHeight - globals.canvasHeight/4
+      },
+      path: {
+        x: globals.canvasWidth/2,
+        y: globals.canvasHeight/2
+      }
+    }
+  ];
 
   this.update = function() {
     this.age++;
-    if  (!(this.age % 100)) {
-      var asteroidCenter = {
-        x: globals.canvasWidth,
-        y: Math.round(globals.canvasHeight * Math.random())
-      };
-      var asteroidPath = {
-        x: Math.round((globals.canvasWidth-400) * Math.random()) + 400,
-        y: Math.round(Math.random() * globals.canvasHeight)
-      };
-      var asteroidVel = {
-        x: -1,
-        y: 0
-      };
-      // Create Asteroid instance
-      this.c.entities.create(Asteroid, { 
-        center: asteroidCenter,
-        vel: asteroidVel,
-        angle: 180,
-        path: asteroidPath,
-        health: 300,
-        size : { x:20 , y:20 }, 
-        color:'magenta'
-      });
+    for (var i = 0; i < this.waves.length; i++) {
+      if (this.age > this.waves[i].times[0] && this.age < this.waves[i].times[1]) {
+        if (!(this.age % 30)) {
+          this.spawnEnemy(this.enemyModels.basic, {
+            center: {
+              x: this.waves[i].center.x,
+              y: this.waves[i].center.y,
+            },
+            path: {
+              x: this.waves[i].path.x,
+              y: this.waves[i].path.y,
+            }
+          });
+        }
+      }      
     }
-  };
-};
-
-// Asteroid class
-var Asteroid = function(game, settings) {
-  this.name = 'Asteroid';
-  this.c = game.c;
-  for (var i in settings) {
-    this[i] = settings[i];
-  }
-  var dice = Math.random();
-  this.angle_vel = Math.random();
-  this.angle_vel = (dice > 0.5) ? this.angle_vel : -this.angle_vel;
-  this.gun = {
-    age: 0,
-    shoot: function(shooter) {
-      var forward = globals.angleToVector(shooter.angle);     
-      var bulletCenter = {
-        x: shooter.center.x + 15 * forward.x,
-        y: shooter.center.y + 15 * forward.y
-      };
-      var bulletVel = {
-        x: 6 * forward.x,
-        y: 6 * forward.y
-      };
-      if (game.c.entities.all(Bullet).length < 40) {
-         game.c.entities.create(Bullet, { 
-          size : { x:3 , y:3 }, 
-          angle: shooter.angle,
-          center: bulletCenter,
-          vel: bulletVel,
-          type: "Enemy",
-          color:"black"
-        });     
+    if (this.age > (this.waves[this.waves.length-1].times[1] + 200)) {
+      if (!this.c.entities.all(Boss).length) {
+        this.spawnBoss(this.enemyModels.boss, {});
       }
     }
   };
 
+  this.spawnBoss = function (model, overrides) {
+    var boss = jQuery.extend(true, {}, model);
+    boss.timestamp = this.age;
+    for (var i in overrides) {
+      boss[i] = overrides[i];
+    }
+    // Create boss instance
+    this.c.entities.create(Boss, { 
+      center: boss.center,
+      vel: boss.vel,
+      angle: boss.angle,
+      angle_vel: boss.angle_vel,
+      health: boss.health,
+      size : boss.size, 
+      color: boss.color,
+      gun: boss.gun
+    }); 
+  }
+
+  this.spawnEnemy = function (model, overrides) {
+    var enemy = jQuery.extend(true, {}, model);
+    enemy.timestamp = this.age;
+    for (var i in overrides) {
+      enemy[i] = overrides[i];
+    }
+    // Create enemy instance
+    this.c.entities.create(Enemy, { 
+      center: enemy.center,
+      vel: enemy.vel,
+      angle: enemy.angle,
+      angle_vel: enemy.angle_vel,
+      path: enemy.path,
+      health: enemy.health,
+      size : enemy.size, 
+      color: enemy.color,
+      gun: enemy.gun
+    });    
+  };
+};
+
+// Boss class
+var Boss = function(game, settings) {
+  this.name = 'Boss';
+  this.c = game.c;
+  for (var i in settings) {
+    this[i] = settings[i];
+  }
+
   this.update = function() {
+    this.angle += this.angle_vel;
+    if (this.angle >= 190 || this.angle <= 170) {
+      this.angle_vel = -this.angle_vel;
+    }
+    this.center.x += this.vel.x;
+    this.center.y += this.vel.y;
+    if (this.center.x <= 700) {
+      this.vel.x = 0;
+      if (this.vel.y === 0) {
+        this.vel.y = (Math.random() >= 0.5) ? -1: 1;
+      } else if (this.center.y >= (globals.canvasHeight - this.size.y)) {
+        this.vel.y = -1;
+      } else if (this.center.y <= this.size.y) {
+        this.vel.y = 1;
+      }
+    }
+
+    if (this.health <= 0) {
+      globals.winGame(this.c);
+    }
+    this.gun.age++;
+    if (!(this.gun.age % 20)) {
+      this.gun.shoot(this);
+    }
+  };
+
+  this.draw = function(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.center.x - this.size.x / 2,
+                 this.center.y - this.size.y / 2,
+                 this.size.x,
+                 this.size.y);
+  };
+};
+
+// Enemy class
+var Enemy = function(game, settings) {
+  this.name = 'Enemy';
+  this.c = game.c;
+  for (var i in settings) {
+    this[i] = settings[i];
+  }
+
+  this.update = function() {
+    this.angle += this.angle_vel;
+    if (this.angle >= 215 || this.angle <= 145) {
+      this.angle_vel -= this.angle_vel * 2;
+    } 
     if (this.center.x > this.path.x) {
       this.center.x -=2;
     } else if (this.center.y === this.path.y) {
@@ -269,12 +508,10 @@ var Asteroid = function(game, settings) {
     this.angle += this.angle_vel;
     if (this.center.x <= 0) {
       globals.score -= 100;
-      document.getElementById('score').innerHTML = globals.score;
       this.c.entities.destroy(this);
     }
     if (this.health <= 0) {
       globals.score += 90;
-      document.getElementById('score').innerHTML = globals.score;
       this.c.entities.destroy(this);
     }
     this.gun.age++;
@@ -284,14 +521,13 @@ var Asteroid = function(game, settings) {
   };
 
   this.draw = function(ctx) {
-    ctx.fillStyle = settings.color;
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.center.x - this.size.x / 2,
                  this.center.y - this.size.y / 2,
                  this.size.x,
                  this.size.y);
   };
 };
-
 
 // Bullet class
 var Bullet = function(game, settings) {
@@ -317,10 +553,10 @@ var Bullet = function(game, settings) {
   };
 
   this.collision = function(other) {
-    if (other.name === 'Asteroid' && this.type !== 'Enemy'){
+    if ((other.name === 'Enemy' && this.type !== 'Enemy') || (other.name === 'Boss' && this.type !== 'Enemy')){
       other.health -= 100;
       globals.score += 10;
-      document.getElementById('score').innerHTML = globals.score;
+      globals.message.txt = "Score: " + globals.score;
       this.c.entities.destroy(this);
     } else if (other.name === 'Player' && this.type === 'Enemy') {
       this.c.entities.destroy(this);
